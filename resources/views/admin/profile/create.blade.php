b9b60a118443145d67bf29d692a9f924b3e0ee18
ezaki <ezaki50ccer@gmail.com>
2021-10-25 09:18:33 +0000

    phplaravel12

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,5 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>MyProfile</title>
-    </head>
     <body>
          {{-- layouts/profile.blade.phpを読み込む --}}
 　　　　@extends('layouts.profile')
