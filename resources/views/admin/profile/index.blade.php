23ab0dff8dde4c9dcdd433422509dfa7d11f97e7
ezaki <ezaki50ccer@gmail.com>
2021-12-14 09:33:38 +0000

    phplaravel16

---
+++ resources/views/admin/profile/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+@section('title', '登録済みプロフィールの一覧')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h2>MYプロフィール</h2>
+
+@endsection
